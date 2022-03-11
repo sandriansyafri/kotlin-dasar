e18ff6a878ab9f37bf39c4d274907a6dc12a4580
sandriannsyafri <sandriannsyafri@gmail.com>
2022-03-11 16:18:21 +0700

    operasi matematika

---
+++ OperasiMatematika.kt
@@ -0,0 +1,49 @@
+fun main(){
+
+      var a: Byte  = 10;
+      var b: Byte  = 10;
+      var c: Byte  = -10;
+      var d: Boolean = true;
+
+      println(a++)
+      println(a++)
+      println(a++)
+      
+      println(b--)
+      println(b--)
+      println(b--)
+
+      println(c)
+      println(!d)
+
+
+      val x :Int = 30;
+      val y :Int = 20;
+
+      val handlePlus :Int =  x + y;
+      val handleMin :Int =  x - y;
+      val handleMultiply :Int =  x * y;
+      val handleDivide :Int =  x / y;
+      val handleMod :Int =  x % y;
+
+      println(handlePlus)
+      println(handleMin)
+      println(handleMultiply)
+      println(handleDivide)
+      println(handleMod)
+
+      var total: Int = 0;
+
+      var brg1 = 1000;
+      var brg2 = 2000;
+      var brg3 = 3000;
+
+      total+= brg1;
+      total+= brg2;
+      total+= brg3;
+      println(total)
+
+
+
+
+}
