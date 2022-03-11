e1125a4af11a305ae0caabbbc199e2551c5ba1cd
sandriannsyafri <sandriannsyafri@gmail.com>
2022-03-11 15:57:38 +0700

    type data range

---
+++ DataRange.kt
@@ -0,0 +1,37 @@
+fun main() {
+
+      //TODO RANGE UP
+      // var rangeUp  = 1..100;
+      // println(rangeUp.count())
+      // println(rangeUp.contains(50))
+      // println(rangeUp.contains(101))
+      // println(rangeUp.first)
+      // println(rangeUp.last)
+      // println(rangeUp.step)
+
+      //TODO RANGE DOWN
+      // var rangeDown  =  100 downTo 1;
+      // println(rangeDown.count())
+      // println(rangeDown.first)
+      // println(rangeDown.last)
+      // println(rangeDown.contains(5))
+      // println(rangeDown.contains(101))
+      // println(rangeDown.step)
+
+      // TODO CHANGE STEP
+      var rangeChangeStep  =  1..100 step 2;
+
+      println(rangeChangeStep.count())
+      println(rangeChangeStep.first)
+      println(rangeChangeStep.last)
+      println(rangeChangeStep.step)
+      println(rangeChangeStep.contains(49))
+      println(rangeChangeStep.contains(50))
+
+
+
+
+      
+
+
+}
