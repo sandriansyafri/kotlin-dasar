4428ff9c9ff48d7e63e421d1fbb7d87c7c37e3cd
sandriannsyafri <sandriannsyafri@gmail.com>
2022-03-11 14:39:56 +0700

    type data string

--- String.kt
+++ String.kt
@@ -1,15 +1,32 @@
 fun main(){
       var name: String = "Sandrian Syafri"
       var address: String = """ 
-            Jl.Guru, 
-            RT 006 
-            RW 002 
-            Kel.Cadika 
-            Kec.Rimbo Tengah
-      """
+            |Jl.Guru, 
+            |RT 006 
+            |RW 002 
+            |Kel.Cadika 
+            |Kec.Rimbo Tengah
+      """.trimMargin()
 
-      println(name);
-      println(address);
+      var address_2: String = """ 
+            >Jl.Guru, 
+            >RT 006 
+            >RW 002 
+            >Kel.Cadika 
+            >Kec.Rimbo Tengah
+      """.trimMargin(">")
+
+      var firstName: String = "Sandrian"
+      var lastName: String = "Syafri"
+      var fullname = "${firstName} ${lastName}"
+      var length_chars : Int = firstName.length + lastName.length;
+
+      println(name)
+      println(address)
+      println("-----------------------")
+      println(address_2)
+      println("-----------------------")
+      println("${fullname} (${length_chars})")
 
 
 
