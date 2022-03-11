289f527f3de7fd806cad7c4d79a85193d18fa4e9
sandriannsyafri <sandriannsyafri@gmail.com>
2022-03-11 12:19:46 +0700

    init & type data Int

---
+++ src/Number.kt
@@ -0,0 +1,24 @@
+fun main(){
+
+      // without underscore in number : 30000000 [ not-readable ]
+
+      // Integer Number
+      // with underscore in number : 30_000_000 [ readable ]
+      var typeByte: Byte = 30 // Byte : [-128 - 127 ]  
+      var typeShort: Short = 168 // Short :  [ -32768 - 32767 ] 
+      var typeInt: Int = 3_000 // Int : [ - 2.147.483,648 (-2^31) -  2.147.483,647 (2^31 - 1) ]
+      var typeLong: Long = 4_000_000L // Long : [ -9.223.327.036.854.775.808 (-2^63) - 9.223.327.036.854.775.808 (2^63 - 1) ]
+
+      // Float Point Number
+      var typeFloat: Float = 30.02F
+      var typeDouble: Double = 3.02;
+
+      println(typeByte)
+      println(typeShort)
+      println(typeInt)
+      println(typeLong)
+
+      println(typeFloat)
+      println(typeDouble)
+
+}
